141254a037ec1fefb3ea9c5bbe7ca8c789d7baeb
rolysr <rolsanchez@yandex.com>
2022-08-08 01:04:51 -0400

    Added main navigation tool that has NavLinks elements for each page created

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   // domain => localhost:3000
   return (
     <div>
+      <MainNavigation />
+
       <Routes>
         <Route path="/" element={<AllMeetupsPage />} />
         <Route path="/new-meetup" element={<NewMeetupPage />} />
