8794ffcf3097214d62178ada58f7e497a157f46c
rolysr <rolsanchez@yandex.com>
2022-08-07 23:26:07 -0400

    Added the project basic pages and basic routing for these pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import Todo from "./components/Todo";
+import { Routes, Route } from "react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
 
 function App() {
+  // domain => localhost:3000
   return (
     <div>
-      <h1>My Todos</h1>
-      <Todo text="Learn React" />
-      <Todo text="Master React" />
-      <Todo text="Explore the full React course" />
+      <Routes>
+        <Route path="/" element={<AllMeetupsPage />} />
+        <Route path="/new-meetup" element={<NewMeetupPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
     </div>
   );
 }
