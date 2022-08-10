3a5310c09c3175c2427dfeb9b350005fe222aa46
rolysr <rolsanchez@yandex.com>
2022-08-10 01:13:05 -0400

    Added Layout component taking advantage of props.children property

--- src/App.js
+++ src/App.js
@@ -3,20 +3,19 @@ import { Routes, Route } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 
 function App() {
   // domain => localhost:3000
   return (
-    <div>
-      <MainNavigation />
+    <Layout>
 
       <Routes>
         <Route path="/" element={<AllMeetupsPage />} />
         <Route path="/new-meetup" element={<NewMeetupPage />} />
         <Route path="/favorites" element={<FavoritesPage />} />
       </Routes>
-    </div>
+    </Layout>
   );
 }
 
